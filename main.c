8ed3b09e40215356a92186c25f84483d159fedb8
David Luis Wiegandt <dlw@phonoforce.com>
2015-11-27 20:44:53 +0100

    n-gram computation (paths) working now

--- main.c
+++ main.c
@@ -1,72 +1,38 @@
 #include <stdio.h>
 #include "edu/humboldt/wbi/graph.h"
 
-int get_ngram(TGraph* g, TArray* array, int array_index, int vertex_index, int n) {
+int get_ngram(TGraph* g, TArray* array, int array_index, int vertex_index, int n_counter, int n) {
+    TVertex* v = graph_get_vertex(g, vertex_index);
     TNGram current_ngram = {
             array_get(array, array_index)
     };
 
-    current_ngram.values[n - 1] = vertex_index;
+    current_ngram.values[n_counter - 1] = vertex_index;
 
-    if (n > 1) {
+    if (n_counter > 1 && v->outdegree > 0) {
         int ngram_count = 0;
-        TVertex* v = graph_get_vertex(g, vertex_index);
         TIterator* it = vertex_get_successor(v);
-
-        if (v->outdegree == 0) { // vertex is a sink, can't complete n-gram
-            return 0;
-        }
 
         while (iterator_has_next(it)) {
             int suc_index = *(int*) iterator_next(it);
 
-            ngram_count += get_ngram(g, array, array_index + ngram_count, suc_index, n - 1);
+            ngram_count += get_ngram(g, array, array_index + ngram_count, suc_index, n_counter - 1, n);
         }
 
-        return ngram_count + get_ngram(g, array, array_index + ngram_count, current_ngram.values[1], 3);
+        return ngram_count;
+    }
+    else if (n_counter == 1 && v->outdegree > 0) {
+        return get_ngram(g, array, array_index + 1, current_ngram.values[n - 2], n, n) + 1;
     }
     else {
-        return 1;
+        return n_counter == 1;
     }
-}
-
-int cmp(TGraph* g, TArray* array, int array_index, int vertex_index, int n, int n_max) {
-    int ngram_count = 0;
-    TVertex* v = graph_get_vertex(g, vertex_index);
-
-    if (array_index >= array->entry_count) {
+    /*else if (n > 1 && v->outdegree == 0) {
         return 0;
     }
-
-    TNGram current_ngram = {
-            array_get(array, array_index)
-    };
-
-    current_ngram.values[n - 1] = vertex_index;
-
-    if (n > 1) {
-        TIterator* it = vertex_get_successor(v);
-
-        if (v->outdegree == 0) { // vertex is a sink, can't complete n-gram
-            return 0;
-        }
-
-        while (iterator_has_next(it)) {
-            int suc_index = *(int*) iterator_next(it);
-
-            ngram_count += cmp(g, array, array_index + ngram_count, suc_index, n - 1, n_max);
-        }
-    }
-    else {
-        if (v->outdegree > 0) {
-            ngram_count++;
-            ngram_count += cmp(g, array, array_index + ngram_count, current_ngram.values[n_max - 2], n_max, n_max);
-        }
-
-        //return 1;
-    }
-
-    return ngram_count;
+    else if (n == 1 && v->outdegree == 0) {
+        return 1;
+    }*/
 }
 
 TArray* fake_cmp(TGraph* g, int n) {
@@ -80,7 +46,7 @@ TArray* fake_cmp(TGraph* g, int n) {
         TVertex* v = graph_get_vertex(g, i);
 
         if (v->indegree == 0) {
-            ngram_count += get_ngram(g, ngrams, ngram_count, i, n);
+            ngram_count += get_ngram(g, ngrams, ngram_count, i, n, n);
         }
     }
 
