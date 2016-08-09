3485d7aab68daf4135be37c3f32defd923f90d11
David Luis Wiegandt <davidwiegandt@outlook.com>
2016-08-09 12:57:49 +0200

    improved crossed platform compilation support

--- src/defines.h
+++ src/defines.h
@@ -1,5 +1,5 @@
 #ifdef _WIN32
 #define __alloca(type, count) alloca(sizeof(type) * count)
 #elif __linux__
-#define __alloca(type, count) type[count]
+#define __alloca(type, count) { type __alloca_var[count]; &__var }
 #endif
